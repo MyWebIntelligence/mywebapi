a88e89d1e045f2caffe0f544128f8cd68a7cd97c
Amar LAKEL <amar@lakel.net>
2026-02-17 07:24:51 +0100

    FallBack dev 02

--- MyWebClient/client/src/api/graphApi.js
+++
@@ -1,4 +0,0 @@
-import client from './client'
-
-export const getGraphData = (landId, params = {}) =>
-  client.get(`/v2/lands/${landId}/graph`, { params }).then((r) => r.data)
--- MyWebClient/client/src/components/EmptyState.jsx
+++
@@ -1,9 +0,0 @@
-export default function EmptyState({ title = 'Aucun résultat', message, action }) {
-  return (
-    <div className="text-center text-muted py-5">
-      <h5>{title}</h5>
-      {message && <p>{message}</p>}
-      {action}
-    </div>
-  )
-}
